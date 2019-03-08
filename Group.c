06b6caf6d6a4ec562f30b71ef8c674b855b6cb8e
student <student@localhost.localdomain>
2019-03-08 12:09:26 -0600

    InitialFile

---
+++ Group.c
@@ -0,0 +1,10 @@
+//Chapters we should to cover: ch02(Variables),ch03(Arrays/Strings),ch04(Print/Scan),ch05(Arithmetic Operators),ch07(Control Flow),ch11(Pointers),
+//ch12(File.IO),ch13(Memory Allocation),ch14(Structs)
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+    
+}
